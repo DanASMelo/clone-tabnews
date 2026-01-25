469fe2494b9591ae8bf707e71195bc3275258939
Danrley Melo <107737232+DanASMelo@users.noreply.github.com>
2026-01-25 23:23:52 +0000

    Ajuste de frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Marina, eu te amo. Desculpa ter te largado no sonho. Tá ❄️ lá fora! Hoje, tá nevando muito!</h1>
+    return <h1>Marina, eu te amo. Me desculpa ter te largado no seu sonho.❄️</h1>
 }
 
 export default Home;
