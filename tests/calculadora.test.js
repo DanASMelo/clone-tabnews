001c0709e483571a89cb69d77ec090d9b16a84d7
Danrley Melo <107737232+DanASMelo@users.noreply.github.com>
2026-02-16 21:51:05 +0000

    incluindo mais testes

--- tests/calculadora.test.js
+++ tests/calculadora.test.js
@@ -9,3 +9,8 @@ test("somar 5 + 100 deveria retornar 105", () => {
   const resultado = calculadora.somar(5, 100);
   expect(resultado).toBe(105);
 });
+
+test("somar banana + 100 deveria retornar 'Erro'", () => {
+  const resultado = calculadora.somar("banana", 100);
+  expect(resultado).toBe("Erro");
+});
